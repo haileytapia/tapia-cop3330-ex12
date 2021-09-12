261528662d7cc6c24b261f0d4c6bc5a20b094f34
Hailey Desktop <haileytapia@knights.ucf.edu>
2021-09-12 10:43:29 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,31 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Hailey Tapia
+ */
+
+package org.example;
+
+import java.util.Scanner;
+
+public class App
+{
+    public static void main(String[] args)
+    {
+        int principal, numYears;
+        double rateOfInterest, simpleInterest, amtAccrued;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the principal: ");
+        principal = input.nextInt();
+
+        System.out.print("Enter the rate of interest: ");
+        rateOfInterest = input.nextDouble();
+
+        System.out.print("Enter the number of years: ");
+        numYears = input.nextInt();
+
+        simpleInterest = principal * (1 + ((rateOfInterest / 100) * numYears));
+
+        System.out.print("After " + numYears + " years at " + rateOfInterest + "%, the investment will be worth $" + (int)Math.round(simpleInterest) + ".");
+    }
+}
