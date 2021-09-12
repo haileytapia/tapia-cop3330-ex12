e70d95c7fcdf7aeb1ecd67420990fbdbe6077b88
Hailey Desktop <haileytapia@knights.ucf.edu>
2021-09-12 10:46:31 -0400

    Updated variables.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,7 +12,7 @@ public class App
     public static void main(String[] args)
     {
         int principal, numYears;
-        double rateOfInterest, simpleInterest, amtAccrued;
+        double rateOfInterest, simpleInterest;
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter the principal: ");
